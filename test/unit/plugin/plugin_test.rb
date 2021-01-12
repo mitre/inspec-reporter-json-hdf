69f1aa5f1a43a657df525d2ed5bff4dad18fd251
Rony Xavier <rxavier@mitre.org>
2021-01-12 07:16:11 -0500

    Fix unit tests

--- test/unit/plugin/plugin_test.rb
+++ test/unit/plugin/plugin_test.rb
@@ -2,7 +2,7 @@ require_relative '../test_helper'
 require_relative '../../../lib/inspec-reporter-json-hdf/reporter'
 
 class InspecPluginTest < Minitest::Test
-  def test_that_csv_exists
+  def test_that_reporter_exists
     refute_nil InspecPlugins::HdfReporter::Reporter
   end
 end
